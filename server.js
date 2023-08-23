34f42b00cf7ee874b8905828394bff17d823d772
Hrik Sinha <dailyhrik@gmail.com>
2023-08-23 11:17:20 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html'); // Create the HTML file
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+    console.log('A user connected');
+    
+    // Handle incoming messages
+    socket.on('message', (data) => {
+        // Broadcast the message to all connected clients
+        io.emit('message', data);
+    });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // Broadcast the message to all connected clients
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
-const PORT = 3000;
+// const PORT = 4000;
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
+
+
